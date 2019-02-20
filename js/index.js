657d029bcc6776f98d58ca831fa69e152badb2db
RodBromberg <rod.bromberg@yahoo.com>
2019-02-20 17:21:14 -0500

    added services page

--- js/index.js
+++ js/index.js
@@ -8,24 +8,61 @@ function closeNav() {
 }
 
 
-// class Dropdown {
-//     constructor(element) {
-//       // assign this.element to the dropdown element
-//       this.element = element;
-//       // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-//       this.button = this.element.querySelector('.dropdown-button')
-//       // assign the reference to the ".dropdown-content" class found in the dropdown element
-//       this.content = this.element.querySelector('.dropdown-content');
-//       // Add a click handler to the button reference and call the toggleContent method.
-//       this.button.addEventListener('click', () => { this.toggleContent() })
-//       // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
-//       // would refer to the button, NOT the current instance of the class. 
-//     }
+class TabLink {
+    constructor(elem) {
+        this.elem = elem;
+        this.elemData = this.elem.dataset.tab;
 
-//     toggleContent() {
-//       // Toggle the ".dropdown-hidden" class off and on
-//       this.content.classList.toggle('dropdown-hidden');
-//     }
-//   }
-//   // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-//   let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+        this.elemData === 0 ? this.items = document.querySelector('.tabcontain') : this.items = document.querySelectorAll(`.tabcontain[data-tab="${this.elemData}"]`)
+
+
+        this.items = Array.from(this.items).map(items => new TabItem(items));
+
+
+        this.elem.addEventListener('click', () => {
+            this.select()
+        });
+    }
+    select() {
+        const link = document.querySelectorAll('.link')
+        Array.from(link).map(tabcontain => { tabcontain.classList.toggle('active-link') });
+
+
+        const tabcontain = document.querySelectorAll('.tabcontain');
+        tabcontain.forEach(tabcontain => { tabcontain.style.display = "none" });
+
+        this.items.map(tabcontain => tabcontain.select());
+
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+    select() {
+        this.element.style.display = "flex";
+    }
+}
+
+
+
+
+
+let links = document.querySelectorAll('.link');
+links = Array.from(links).map(link => new TabLink(link));
+links[0].select();
+
+
+// let links = document.querySelectorAll('.link')
+// links = Array.from(links).forEach((links) => {
+//     new TabItem(links)
+// })
+// links[0].select()
+
+
+// links.forEach(link => new TabLink(link))
+// links[0].selectTab()
+// links[1].selectTab()
+// links[2].selectTab()
+// links[3].selectTab()
